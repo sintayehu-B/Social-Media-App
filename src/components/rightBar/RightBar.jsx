87b43a7252b54b7ac1964f204612f483b99d4f24
sintayehu-B <61502710+sintayehu-B@users.noreply.github.com>
2022-07-06 13:18:21 +0300

    finished contructing rightBar component

--- src/components/rightBar/RightBar.jsx
+++ src/components/rightBar/RightBar.jsx
@@ -2,6 +2,81 @@ import './rightBar.css'
 
 export default function RightBar() {
   return (
-    <div className="rightBarContainer">RightBar</div>
+    <div className="rightBarContainer">
+      <div className="rightBarWrapper">
+        <div className="birthdayContainer">
+
+          <img src="/assets/gift-removebg-preview.png" alt="" className="birthdayImage" />
+          <span className="birthdayText"> <b>Maruf Sherifa</b> and <b>4 other friends</b> have a birthday today.</span>
+        </div>
+        <img src="assets/Post/blockchain.png" alt="" className="rightBarAd" />
+        <h4 className="rightBarTitle"> Online Friends</h4>
+        <ul className="rightBarFriendList">
+          <li className="rightBarFriends">
+            <div className="rightBarProfileContainer">
+              <img src="assets/Person/sintayehu.jpg" alt="" className="rightBarProfileImage" />
+              <span className="rightBarOnline"></span>
+            </div>
+              <span className="rightBarUserName"> Sintayehu Sermessa</span>
+          </li>
+          <li className="rightBarFriends">
+            <div className="rightBarProfileContainer">
+              <img src="assets/Person/sintayehu.jpg" alt="" className="rightBarProfileImage" />
+              <span className="rightBarOnline"></span>
+            </div>
+              <span className="rightBarUserName"> Sintayehu Sermessa</span>
+          </li>
+          <li className="rightBarFriends">
+            <div className="rightBarProfileContainer">
+              <img src="assets/Person/sintayehu.jpg" alt="" className="rightBarProfileImage" />
+              <span className="rightBarOnline"></span>
+            </div>
+              <span className="rightBarUserName"> Sintayehu Sermessa</span>
+          </li>
+          <li className="rightBarFriends">
+            <div className="rightBarProfileContainer">
+              <img src="assets/Person/sintayehu.jpg" alt="" className="rightBarProfileImage" />
+              <span className="rightBarOnline"></span>
+            </div>
+              <span className="rightBarUserName"> Sintayehu Sermessa</span>
+          </li>
+          <li className="rightBarFriends">
+            <div className="rightBarProfileContainer">
+              <img src="assets/Person/sintayehu.jpg" alt="" className="rightBarProfileImage" />
+              <span className="rightBarOnline"></span>
+            </div>
+              <span className="rightBarUserName"> Sintayehu Sermessa</span>
+          </li>
+          <li className="rightBarFriends">
+            <div className="rightBarProfileContainer">
+              <img src="assets/Person/sintayehu.jpg" alt="" className="rightBarProfileImage" />
+              <span className="rightBarOnline"></span>
+            </div>
+              <span className="rightBarUserName"> Sintayehu Sermessa</span>
+          </li>
+          <li className="rightBarFriends">
+            <div className="rightBarProfileContainer">
+              <img src="assets/Person/sintayehu.jpg" alt="" className="rightBarProfileImage" />
+              <span className="rightBarOnline"></span>
+            </div>
+              <span className="rightBarUserName"> Sintayehu Sermessa</span>
+          </li>
+          <li className="rightBarFriends">
+            <div className="rightBarProfileContainer">
+              <img src="assets/Person/sintayehu.jpg" alt="" className="rightBarProfileImage" />
+              <span className="rightBarOnline"></span>
+            </div>
+              <span className="rightBarUserName"> Sintayehu Sermessa</span>
+          </li>
+          <li className="rightBarFriends">
+            <div className="rightBarProfileContainer">
+              <img src="assets/Person/sintayehu.jpg" alt="" className="rightBarProfileImage" />
+              <span className="rightBarOnline"></span>
+            </div>
+              <span className="rightBarUserName"> Sintayehu Sermessa</span>
+          </li>
+        </ul>
+      </div>
+    </div>
   )
 }
